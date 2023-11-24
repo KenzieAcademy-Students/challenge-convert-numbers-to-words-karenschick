b2b46ae06b01bca207e8026ebb71b5e0c125de27
kenzieadmin <kenzieadmin@users.noreply.github.com>
2023-11-24 17:31:55 +0000

    deploy: 6ede0467b884c5e784cb41c92400d4d5837ed1c5

--- script.js
+++ script.js
@@ -34,14 +34,28 @@ const tens = [
 ];
 
 function convertNumbersToWords(number) {
-  if (number === 100) return "one hundred";
+  if (number === 100000) return "one hundred thousand";
   if (number < 20) {
     return ones[number - 1];
   }
   if (number < 100) {
     let ten = Math.floor(number / 10);
     let tenRemainder = number % 10;
-    return tens[ten] + (tenRemainder ? `-${ones[tenRemainder - 1]}` : "");
+    return `${tens[ten]}${tenRemainder ? `-${ones[tenRemainder - 1]}` : ""}`;
+  }
+  if (number < 1000) {
+    let hundred = Math.floor(number / 100);
+    let hundredRemainder = number % 100;
+    return `${ones[hundred - 1]} hundred${
+      hundredRemainder ? ` and ${convertNumbersToWords(hundredRemainder)}` : ""
+    }`;
+  }
+  if (number < 100000) {
+    let thousand = Math.floor(number / 1000);
+    let thousandRemainder = number % 1000;
+    return `${convertNumbersToWords(thousand)} thousand${
+      thousandRemainder ? ` ${convertNumbersToWords(thousandRemainder)}` : ""
+    }`;
   }
 }
 
@@ -49,7 +63,7 @@ const convertedWordsDiv = document.getElementById("converted-words");
 
 const convertedWords = [];
 
-for (let i = 1; i <= 100; i++) {
+for (let i = 1; i <= 100000; i++) {
   convertedWords.push(convertNumbersToWords(i));
 }
 
